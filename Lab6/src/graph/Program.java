947de57d65e4f98c99ff4df24faf2cb57f36be95
knyznar <katarzyna.nyznar@gmail.com>
2018-11-17 19:01:45 +0100

    graph

--- Lab6/src/graph/Program.java
+++ Lab6/src/graph/Program.java
@@ -1,25 +1,17 @@
 package graph;
 
 import javax.swing.*;
-import java.awt.*;
-public class Program extends JFrame {
 
-    private static final int N = 500;
-    private static final int A = 1;
-    private static final int B = 0;
-    private static final int C = 0;
+public class Program extends JFrame {
+    public Program() {
+        JFrame frame = new JFrame("Graph Drawer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new GraphPanel());
+        frame.pack();
+        frame.setVisible(true);
+    }
 
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-
-            @Override
-            public void run() {
-                JFrame frame = new JFrame("Quadratic Slider");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.add(new GraphPanel());
-                frame.pack();
-                frame.setVisible(true);
-            }
-        });
+        Program program = new Program();
     }
 }
