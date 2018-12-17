5d857d0f4480c4c13591e8b9ece04fad236d4940
Katarzyna <katarzyna.nyznar@gmail.com>
2018-12-17 12:23:29 +0100

    lab10

---
+++ Lab10/src/lab10/EchoServer.java
@@ -0,0 +1,52 @@
+package lab10;
+
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(6666);
+        } catch (IOException e) {
+            System.out.println("Could not listen on port: 6666");
+            System.exit(-1);
+        }
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        while (true){
+
+            final Socket socket = serverSocket.accept();
+            Runnable connection = new Runnable() {
+
+                @Override
+                public void run() {
+                    try {
+
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        bufferedWriter.flush();
+                        String line = bufferedReader.readLine();
+                        while (line!= null){
+                            bufferedWriter.write(line);
+                            bufferedWriter.write("\n");
+                            bufferedWriter.flush();
+                            line = bufferedReader.readLine();
+                        }
+                        socket.close();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+
+                    }
+                }
+
+
+
+            };
+            executorService.submit(connection);
+        }
+    }
+}
